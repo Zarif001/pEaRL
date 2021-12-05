44e1995d30acf88c9df87bafdc13fe641c2b05a7
Zarifjon <zarifjon@gmail.com>
2021-12-05 22:37:44 +0500

    js_3

---
+++ app/src/script/all.js
@@ -0,0 +1,33 @@
+// Header
+const headerImg = document.querySelector('.header__info-img'),
+  headerInfo = document.querySelector('.header__info'),
+  headerContent = document.querySelector('.header__content')
+
+headerImg.addEventListener('click', (e) => {
+  e.preventDefault()
+  headerInfo.classList.add('active')
+})
+headerInfo.addEventListener('click', () => {
+  headerInfo.classList.remove('active')
+})
+// Main 
+const mainImg = document.querySelectorAll('.main__kitchen-picture')
+mainImg.forEach(list => {
+  list.addEventListener('click', () => {
+    if (list.classList.contains('active')) {
+      list.classList.remove('active')
+    } else {
+      list.classList.add('active')
+    }
+  })
+})
+
+
+
+
+
+
+
+
+
+
