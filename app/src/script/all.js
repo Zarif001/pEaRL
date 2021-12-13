a6a633ab9bc96bbc2b96f4ec006ff00e93acda19
zarif <zarif@gmail.com>
2021-12-13 18:57:24 +0300

    js

--- app/src/script/all.js
+++ app/src/script/all.js
@@ -23,30 +23,40 @@ mainImg.forEach(list => {
 })
 
 const slide = document.querySelector('.main__menu-picture'),
- slideParent = document.querySelectorAll('.main__menu-aboutPicture '),
+ slideParent = document.querySelectorAll('.main__menu-aboutPicture'),
  slideHeight = slide.clientHeight,
  slideWidth = slide.clientWidth,
  activeSlide = 0
+ 
+ console.log(slideParent);
 
 slide.style = `
 position:relative;
 height:${slideHeight}px;
 overflow:hidden;
 `
-slideParent.forEach(item => {
-  for(let i = 0; i<item.length; i++){
+function time() {
+  slideParent.forEach((item,i) => {
     item.style = `
-    position:absolute;
-    width: ${slideWidth}px;
-    height: ${slideHeight}px;
-    `
-    if(i != activeSlide){
-      item.style.transform = `translateX(${slideWidth}px)`
-    }
+      position:absolute;
+      width: ${slideWidth}px;
+      height: ${slideHeight}px;
+      `
+     
+        if(i == 0){
+          item.style.transform = `translateX(0px)`
+        }else {
+          item.style.transform = `translateX(${-slideWidth}px)`
+        }
     
-  } 
+      
+  
+  })
+}
 
-})
+time()
+setInterval(time, 1000);
+// slideParent[activeSlide].style.transform = `translateX(0px)`
 
 
 
